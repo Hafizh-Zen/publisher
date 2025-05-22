05daf3c3d0434c923427414cd171105aa8c31fd6
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2025-05-23 00:58:21 +0700

    Edit Product service methods to call notify after create/delete.

--- src/service/product.rs
+++ src/service/product.rs
@@ -1,21 +1,29 @@
 use rocket::http::Status;
 use rocket::serde::json::Json;
+
+use bambangshop::{Result, compose_error_response};
+use crate::model::product::Product;
+use crate::repository::product::ProductRepository;
+
 use super::notification::NotificationService;
 
 pub struct ProductService;
- 
+
  impl ProductService {
      pub fn create(mut product: Product) -> Result<Product> {
          product.product_type = product.product_type.to_uppercase();
          let product_result: Product = ProductRepository::add(product);
- 
+
+         NotificationService.notify(&product_result.product_type, "CREATED", 
+            product_result.clone());
+
          return Ok(product_result);
      }
- 
+
      pub fn list() -> Result<Vec<Product>> {
          return Ok(ProductRepository::list_all());
      }
- 
+
      pub fn read(id: usize) -> Result<Product> {
          let product_opt: Option<Product> = ProductRepository::get_by_id(id);
          if product_opt.is_none() {
@@ -26,7 +34,7 @@ pub struct ProductService;
          }
          return Ok(product_opt.unwrap());
      }
- 
+
      pub fn delete(id: usize) -> Result<Json<Product>> {
          let product_opt: Option<Product> = ProductRepository::delete(id);
          if product_opt.is_none() {
@@ -37,9 +45,10 @@ pub struct ProductService;
          }
          let product: Product = product_opt.unwrap();
  
+         NotificationService.notify(&product.product_type, "DELETED", product.clone());
          return Ok(Json::from(product));
      }
- 
+
      pub fn publish(id: usize) -> Result<Product> {
          let product_opt: Option<Product> = ProductRepository::get_by_id(id);
          if product_opt.is_none() {
@@ -49,8 +58,8 @@ pub struct ProductService;
              ));
          }
          let product: Product = product_opt.unwrap();
-     
+
          NotificationService.notify(&product.product_type, "PROMOTION", product.clone());
          return Ok(product);
-     }    
- }
+     }  
+    }
