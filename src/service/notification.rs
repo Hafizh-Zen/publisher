56a431ddeb971c0ae5ac2c67eb75f7a6d74aeae0
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2025-05-23 00:56:10 +0700

    Implement notify function in Notification service to notify each Subscriber.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -15,7 +15,6 @@ use bambangshop::{Result, compose_error_response};
          let product_type_str: &str = product_type_upper.as_str();
          let subscriber_result: Subscriber = SubscriberRepository::add(product_type_str, subscriber);
          return Ok(subscriber_result);
-      
      }
 
      pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
@@ -29,5 +28,24 @@ use bambangshop::{Result, compose_error_response};
             ));
         }
         return Ok(result.unwrap());
-    }    
+       
+    }
+
+    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
+         let mut payload: Notification = Notification {
+             product_title: product.clone().title,
+             product_type: String::from(product_type),
+             product_url: product.clone().get_url(),
+             subscriber_name: String::from(""),
+             status: String::from(status),
+         };
+     
+         let subscribers: Vec<Subscriber> = SubscriberRepository::list_all(product_type);
+         for subscriber in subscribers {
+             payload.subscriber_name = subscriber.clone().name;
+             let subscriber_clone = subscriber.clone();
+             let payload_clone = payload.clone();
+             thread::spawn(move || subscriber_clone.update(payload_clone));
+         }
+     }    
  }
